a2efa501d1c543977ad4119769ecf7549d5fc96c
Jaberofficial <jaberhossainmojumderononto02@gmail.com>
2026-09-04 22:46:14 +0600

    Shop_Return-Process_Privacy-Policy_Refund-Policy_Terms & Conditions_About-us_Contact-Us Page Mastering Done.

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -16,4 +16,39 @@ class FrontendController extends Controller
    {
     return view ('frontend.product-details');
    }
+   public function shopProducts()
+   {
+      return view('frontend.shop');
+   }
+
+   public function privacyPolicy()
+   {
+      return view('frontend.privacy-policy');
+   }
+
+   public function termsConditions()
+   {
+      return view('frontend.terms-conditions');
+   }
+
+      public function refundPolicy()
+   {
+      return view('frontend.refund-policy');
+   }
+
+      public function paymentPolicy()
+   {
+      return view('frontend.payment-policy');
+   }
+
+      public function aboutUs()
+   {
+      return view('frontend.about-us');
+   }
+
+   public function contactUs()
+   {
+      return view('frontend.contact-us');
+   }
 }
+
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[FrontendController::class, 'index']);
 Route::get('/product-details',[FrontendController::class, 'productDetails']);
+Route::get('/shop',[FrontendController::class, 'shopProducts']);
+Route::get('/privacy-policy',[FrontendController::class, 'privacyPolicy']);
+Route::get('/terms-conditions',[FrontendController::class, 'termsConditions']);
+Route::get('/refund-policy',[FrontendController::class, 'refundPolicy']);
+Route::get('/payment-policy',[FrontendController::class, 'paymentPolicy']);
+Route::get('/about-us',[FrontendController::class, 'aboutUs']);
+Route::get('/contact-us',[FrontendController::class, 'contactUs']);
